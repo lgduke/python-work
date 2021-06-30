85ea909c191113f9475b73c2aa2434f1c0658f66
Duke H <lgduke.us@gmail.com>
2021-06-30 22:34:10 +0900

    In home, work in selenimum

--- webscrapping-basic/13_selenium.py
+++ webscrapping-basic/13_selenium.py
@@ -1,4 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-browser = webdriver.Chrome("./chromedriver.exe")
+# browser = webdriver.Chrome("C:\\Users\\user\\python-work\\python-work\\chromedriver.exe")
+browser = webdriver.Chrome()
 browser.get("http://naver.com")
+elem = browser.find_element_by_class_name("link_login")
+elem.click()
+browser.back()
+browser.forward()
+elem = browser.find_element_by_id("query")  
+elem.send_keys("나도코딩")
+elem.send_keys(Keys.ENTER)
